9d94d905d9a5b813078470d74f4c29686e8fd0c1
Jawad Muhammadi <muhammadijawad10@gmail.com>
2018-12-12 14:09:16 -0800

    showBoard function displays board correctly

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -13,4 +13,11 @@ var board = {
 
 function initializeBoard(id, p) {
     board[id] = p.toUpperCase();
+};
+
+function showBoard() {
+    console.log(`\n ${board[1]} | ${board[2]} | ${board[3]}\n ---------\n ${board[4]} | ${board[5]} | ${board[6]} \n ---------\n ${board[7]} | ${board[8]} | ${board[9]}\n`);
 }
+
+
+
